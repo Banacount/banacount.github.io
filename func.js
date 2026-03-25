69b39207460532aefc4280b1276cefd220f12e1f
Banacount <uniquejmal@gmail.com>
2026-03-25 19:03:02 +0800

    Yeah jus a lil bit of yappyville

--- func.js
+++ func.js
@@ -1,1 +1,17 @@
 console.log("What the helly!");
+let closeBtns = document.querySelectorAll(".window-action button");
+
+closeBtns.forEach((element) => {
+    element.addEventListener('click', () => {
+        console.log("Closed the window.");
+        element.parentElement.parentElement.remove();
+    })
+})
+
+let isBana = false;
+const myName = document.getElementById("myName");
+document.getElementById("myProfile").addEventListener('click', () => {
+    isBana = !isBana;
+    if (isBana) myName.innerHTML = "Banacount";
+    else myName.innerHTML = "Johval";
+})
