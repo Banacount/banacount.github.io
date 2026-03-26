f614b4ab83c98757a6851caf23c1649f4e67feed
Banacount <uniquejmal@gmail.com>
2026-03-26 21:36:58 +0800

    Added a lil bit of SPA in there yk

--- func.js
+++ func.js
@@ -1,17 +1,55 @@
-console.log("What the helly!");
-let closeBtns = document.querySelectorAll(".window-action button");
 
-closeBtns.forEach((element) => {
-    element.addEventListener('click', () => {
-        console.log("Closed the window.");
-        element.parentElement.parentElement.remove();
-    })
-})
+// General Variables
+let eventListeners = []
 
+// Load home
 let isBana = false;
-const myName = document.getElementById("myName");
-document.getElementById("myProfile").addEventListener('click', () => {
-    isBana = !isBana;
-    if (isBana) myName.innerHTML = "Banacount";
-    else myName.innerHTML = "Johval";
-})
+const pageView = document.getElementById("pageView");
+const loadHome = async () => {
+    try {
+        const getHome = await fetch('./home.html');
+        if (!getHome.ok) throw new Error("cannot get home.html content.");
+
+        const getContent = await getHome.text();
+        pageView.innerHTML = getContent
+
+        // Refer to the element and start a controller
+        const myName = document.getElementById("myName");
+        const event1 = new AbortController();
+
+        // Start the listener
+        document.getElementById("myProfile").addEventListener('click', () => {
+            isBana = !isBana;
+            if (isBana) myName.innerHTML = "Banacount";
+            else myName.innerHTML = "Johval";
+        }, { 'signal': event1.signal });
+        // Push the controller on the array so we could get rid of it later
+        eventListeners.push(event1);
+
+        // Window thingys
+        let closeBtns = document.querySelectorAll(".window-action button");
+        closeBtns.forEach((element) => {
+            const controller = new AbortController();
+            element.addEventListener('click', () => {
+                console.log("Closed the window.");
+                element.parentElement.parentElement.remove();
+            }, { 'signal': controller.signal })
+
+            eventListeners.push(controller);
+        })
+
+    } catch (error) {
+        pageView.innerHTML = `<p>${error}</p>`;
+    }
+}
+
+loadHome();
+/*
+document.addEventListener('keydown', (e) => {
+    if (e.code == "KeyS") {
+        console.log("test clear");
+        eventListeners.map((con) => { con.abort() });
+        eventListeners = [];
+    }
+});
+*/
