20cc3d8acb9bcd9e722533c27dc1422a7aae1afe
Banacount <uniquejmal@gmail.com>
2026-03-30 19:37:10 +0800

    Added a lil bit of modularity ykyk frfr

--- func.js
+++ func.js
@@ -1,10 +1,25 @@
+import { loadTemp } from "./renderer.js";
 
-// General Variables
+// My event listeners
 let eventListeners = []
 
-// Load home
+// General Variables
 let isBana = false;
 const pageView = document.getElementById("pageView");
+const searchBtn = document.getElementById("searchBtn");
+const urlInput = document.getElementById("pageUrlInput");
+
+// Log error yeah
+const logError = (error_msg) => {
+    const errorElement = document.getElementById("errorLogEl");
+    errorElement.innerHTML = error_msg;
+};
+// Unload the event listeners list
+const unloadListeners = () => {
+    eventListeners.map((con) => { con.abort() });
+    eventListeners = [];
+};
+// Load home
 const loadHome = async () => {
     try {
         const getHome = await fetch('./home.html');
@@ -25,26 +40,29 @@ const loadHome = async () => {
         }, { 'signal': event1.signal });
         // Push the controller on the array so we could get rid of it later
         eventListeners.push(event1);
-
-        // Window thingys
-        let closeBtns = document.querySelectorAll(".window-action button");
-        closeBtns.forEach((element) => {
-            const controller = new AbortController();
-            element.addEventListener('click', () => {
-                console.log("Closed the window.");
-                element.parentElement.parentElement.remove();
-            }, { 'signal': controller.signal })
-
-            eventListeners.push(controller);
-        })
-
     } catch (error) {
         pageView.innerHTML = `<p>${error}</p>`;
     }
 }
 
+function removeWinElement(element) {
+    element.parentElement.parentElement.remove();
+    console.log("Closed the window.");
+}
+window.removeWinElement = removeWinElement;
+
 loadHome();
+searchBtn.addEventListener('click', () => {
+    unloadListeners();
+    loadTemp(`./${urlInput.value}.html`, (data) => {
+        if (data != 1) {
+            pageView.innerHTML = data;
+            logError("");
+        } else logError(`Cannot access ${urlInput.value} page`);
+    });
+})
 /*
+testing: if the event listener list works
 document.addEventListener('keydown', (e) => {
     if (e.code == "KeyS") {
         console.log("test clear");
--- renderer.js
+++ renderer.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+export const loadTemp = async (templateName, valueCall) => {
+    try {
+        let getTemp = await fetch(templateName);
+        if (!getTemp.ok) getTemp = await fetch(`./pages/${templateName}`);
+        if (!getTemp.ok) throw new Error(`cannot get ${templateName} content.`);
+
+        const getContent = await getTemp.text();
+        valueCall(getContent);
+    } catch (error) {
+      console.error("An error happened: " + error);
+      valueCall(1);
+      return 1;
+    }
+}
